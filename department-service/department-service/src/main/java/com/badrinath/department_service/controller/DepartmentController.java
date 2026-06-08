6a6026527b0e7789538a16b05d5bf51532abdf73
Rudroju Badrinath <rudrojubadrinath27@gmail.com>
2026-06-08 10:57:12 +0530

    add API gateway

--- department-service/department-service/src/main/java/com/badrinath/department_service/controller/DepartmentController.java
+++ department-service/department-service/src/main/java/com/badrinath/department_service/controller/DepartmentController.java
@@ -1,12 +1,12 @@
 package com.badrinath.department_service.controller;
 
+import com.badrinath.department_service.client.employeeClient;
 import com.badrinath.department_service.model.Department;
 import com.badrinath.department_service.repository.DepartmentRepository;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -14,6 +14,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
+
+    @Autowired
+    private employeeClient employeeClient;
 
     private static final Logger logger
             = (Logger) LoggerFactory.getLogger(DepartmentController.class);
@@ -39,6 +42,16 @@ public class DepartmentController {
     public void add(@RequestBody Department department) {
         logger.info("Department add: {}",department);
         departmentRepository.addDepartment(department);
+    }
+
+    @GetMapping("/with-employees")
+    public List<Department> findAllByDepartmentId(){
+        List<Department> departments = departmentRepository.findAll();
+        departments.forEach(department -> {
+            department.setEmployees(
+                    employeeClient.findbydepartment(department.getId()));
+        });
+        return departments;
     }
 
 }
